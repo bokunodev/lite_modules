c3e079fa7dbb457b86532711cecc4194becf9ac3
bokunodev <54968427+bokunodev@users.noreply.github.com>
2020-07-03 05:14:49 +0700

    Update eofnewline.lua

--- plugins/eofnewline.lua
+++ plugins/eofnewline.lua
@@ -3,25 +3,28 @@ local command = require "core.command"
 local Doc = require "core.doc"
 
 local function eof_newline(doc)
-  local eofl = #doc.lines
-  for i = eofl,1,-1 do
-    if (string.match(doc.lines[i],"^%s*$") ~= nil) then
-      table.remove(doc.lines,i)
-    else
-      break
-    end
-  end
-  table.insert(doc.lines,"")
+	local leof,neof = #doc.lines,#doc.lines
+	for i = leof,1,-1 do
+		if not string.match(doc.lines[i],"^%s*$") then
+			break
+		end
+		neof = i
+	end
+	if neof ~= leof then
+		doc:remove(neof,1,leof,math.huge)
+		return
+	end
+	doc:insert(leof,math.huge,"\n")
 end
 
 command.add("core.docview", {
-  ["eof-newline:eof-newline"] = function()
-    eof_newline(core.active_view.doc)
-  end,
+	["eof-newline:eof-newline"] = function()
+		eof_newline(core.active_view.doc)
+	end,
 })
 
 local save = Doc.save
 Doc.save = function(self, ...)
-  eof_newline(self)
-  save(self, ...)
+	eof_newline(self)
+	save(self, ...)
 end
