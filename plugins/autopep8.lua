748cad5e49e073975bf18fc6537d6e23864f8f44
bokunodev <bokunocode@gmail.com>
2020-07-28 21:39:49 +0700

    update Tue 28 Jul 2020 02:39:49 PM UTC

---
+++ plugins/autopep8.lua
@@ -0,0 +1,40 @@
+local core = require "core"
+local command = require "core.command"
+local keymap = require "core.keymap"
+
+local user = require("user")
+
+local error_msg = "ERROR line:%s co:%s %s"
+
+local autopep8_flag = user.autopep8_flag or "-i -j 2 -p 2 --experimental"
+local pycodestyle_flag = user.pycodestyle_flag or ""
+
+local fmt_cmd = "%s %s %s 2>&1"
+
+local function do_format(cmd,flag,doc)
+  local file = doc.filename
+  local p = io.popen(string.format(fmt_cmd, cmd,flag,file))
+  local out = p:read('*a')
+  local status = {p:close()}
+  local pl,pc,_,_ = doc:get_selection()
+  if status[3] == 0 then
+    doc:load(file)
+    doc:set_selection(pl,pc)
+    return
+  elseif status[3] == 127 then
+    core.error("Command '"..cmd.."' not found in the system")
+    return
+  end
+  local line,col,msg = out:match(':(%d-):(%d-):%s+%w+%s+([^\n]+)')
+  doc:set_selection(line,col)
+  core.error(string.format(error_msg,line,col,msg))
+end
+
+command.add("core.docview", {
+  ["python:check"] = function()
+    do_format("pycodestyle",pycodestyle_flag,core.active_view.doc)
+  end,
+  ["python:format"] = function()
+    do_format("autopep8",autopep8_flag,core.active_view.doc)
+  end,
+})
