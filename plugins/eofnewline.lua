67c171fc31b6203bd054c061a4ad6274a3dcf07f
abdul <bokunocode@gmail.com>
2024-01-24 12:22:41 +0700

    fix for #1

--- plugins/eofnewline.lua
+++ plugins/eofnewline.lua
@@ -1,19 +1,19 @@
+-- mod-version:3 -- lite-xl 2.0
 local core = require "core"
 local command = require "core.command"
 local Doc = require "core.doc"
 
 local function eof_newline(doc)
-    local leof,neof = #doc.lines,#doc.lines
-    for i = leof,1,-1 do
-        if not string.match(doc.lines[i],"^%s*$") then break end
-        neof = i
-    end
-    local eol,_ = string.find(doc.lines[neof],"\n")
-    if eol then
-        doc:remove(neof,eol,math.huge,math.huge)
-        return
-    end
-    doc:insert(neof,math.huge,"\n")
+	local leof, neof = #doc.lines, #doc.lines
+	for i = leof, 1, -1 do
+		if not string.match(doc.lines[i], "^%s*$") then break end
+		neof = i
+	end
+	if neof ~= leof then
+		doc:remove(neof, 1, leof, math.huge)
+		return
+	end
+	if "\n" ~= doc.lines[leof] then doc:insert(leof, math.huge, "\n") end
 end
 
 command.add("core.docview", {
@@ -23,7 +23,7 @@ command.add("core.docview", {
 })
 
 local save = Doc.save
-    Doc.save = function(self, ...)
+function Doc:save(...)
     eof_newline(self)
     save(self, ...)
 end
